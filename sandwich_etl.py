550ac87dc8cd414e5d42a934bd9823d7660a131c
cjchz2 <ccunningham9191@gmail.com>
2021-06-08 02:17:59 +0000

    adding etl sandwich to main branch

--- sandwich_etl.py
+++ sandwich_etl.py
@@ -38,7 +38,7 @@ today_offset = str(now_offset.date())
 time_now_int = int(str(datetime.time(now_offset)).split('.')[0].replace(':',''))
 
 for bucket in s3_bucket.objects.all():
-	if today_offset in str(bucket.key) and int(bucket.key.split('%')[1].split(' ')[1].split('-')[0].replace(':', '')) >= time_now_int - 2000 :
+	if today_offset in str(bucket.key) and int(bucket.key.split('%')[1].split(' ')[1].split('-')[0].replace(':', '')) >= time_now_int - 3000 :
 		s3_resource.Object('sandwich-tweet-chris-cunningham',
 			bucket.key).download_file(Filename = bucket.key)
 
@@ -51,13 +51,15 @@ for bucket in s3_bucket.objects.all():
 
 cur = conn.cursor()
 
-file_path_collection = glob.glob('/home/ec2-user/tweet_folder/*.txt')
+file_path_collection = glob.glob('/home/ec2-user/tweet_folder/sandwich_tweet%*')
 for file_path in file_path_collection:
 	with open(file_path) as file:
-		tweet_info = file.read()
-		tweet_list = tweet_info.split('^&*>*&^')
-		tweet_list[3] = int(tweet_list[3])
-		cur.execute(sql_statement, tweet_list)
-		conn.commit()
-
+		try:
+			tweet_info = file.read()
+			tweet_list = tweet_info.split('^&*>*&^')
+			tweet_list[3] = int(tweet_list[3])
+			cur.execute(sql_statement, tweet_list)
+			conn.commit()
+		except:
+			print("this file is an issue: " + tweet_list[1])
 cur.close()
