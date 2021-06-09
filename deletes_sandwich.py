733c2c238d5ed2fe6f600c72132340fe78595f3e
cjchz2 <ccunningham9191@gmail.com>
2021-06-09 02:27:03 +0000

    Updating file references

--- deletes_sandwich.py
+++ deletes_sandwich.py
@@ -1,7 +1,7 @@
 import glob
 import os
 
-file_path = glob.glob('/home/ec2-user/tweet_folder/sandwich_tweet%*')
+file_path = glob.glob('/home/ec2-user/sandwich_tweet%*')
 
 for file in file_path:
     os.remove(file)
