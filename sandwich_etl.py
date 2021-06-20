2b82fd9d826e6f9940ef4921cd9c951cdaf1f21f
cjchz2 <38957891+cjchz2@users.noreply.github.com>
2021-06-20 13:12:18 -0500

    Update sandwich_etl.py

--- sandwich_etl.py
+++ sandwich_etl.py
@@ -17,29 +17,31 @@ password_str = credential_list[1]
 
 #Creating connection to the PostgreSQL database.
 conn = psycopg2.connect(database = 'sandwich_tweets',
-		user = user_name_str.strip(),
-		password = password_str.strip(),
-		host = 'sandwich-tweets.cxors8ly0k2i.us-east-2.rds.amazonaws.com',
-		port = '5432'
-)
+		        user = user_name_str.strip(),
+		        password = password_str.strip(),
+		        host = 'sandwich-tweets.cxors8ly0k2i.us-east-2.rds.amazonaws.com',
+		        port = '5432')
 
-sql_statement = 'INSERT INTO "sandwich_tweets"."tweets"(TEXT,USERNAME,DATE_TWEETED,ID)\
-		VALUES (%s, %s, %s, %s)'
-
+sql_statement = ('INSERT INTO "sandwich_tweets"."tweets"(ID,TEXT,USERNAME,TWEET_DATE,PULLED_DATE)'
+                 'VALUES (%s, %s, %s, %s, %s)'
+                 'ON CONFLICT DO NOTHING')
 #Creating connection to the s3 bucket.
 s3_resource = boto3.resource('s3')
 s3_bucket = s3_resource.Bucket('sandwich-tweet-chris-cunningham')
 
 #This is going to be used to grab data frome the last hour.
-six_hour_delta = timedelta(hours = 6)
-now_offset = datetime.now().astimezone(timezone('US/Central'))
-today_offset = str(now_offset.date())
-time_now_int = int(str(datetime.time(now_offset)).split('.')[0].replace(':',''))
+#six_hour_delta = timedelta(hours = 6)
+now_datetime = datetime.now()
+#today_offset = str(now_offset.date())
+#time_now_int = int(str(datetime.time(now_offset)).split('.')[0].replace(':',''))
 
 for bucket in s3_bucket.objects.all():
-	if today_offset in str(bucket.key) and int(bucket.key.split('%')[1].split(' ')[1].split('-')[0].replace(':', '')) >= time_now_int - 3000 :
-		s3_resource.Object('sandwich-tweet-chris-cunningham',
-			bucket.key).download_file(Filename = bucket.key)
+    file_datetime_str = bucket.key[15:34]
+    file_datetime = datetime.strptime(file_datetime_str, "%Y-%m-%d %H:%M:%S")
+    print(file_datetime)
+    if file_datetime > now_datetime - timedelta(hours = 1):
+        print('hell yeah')
+        s3_resource.Object('sandwich-tweet-chris-cunningham',bucket.key).download_file(Filename = bucket.key)
 
 
 #Taking the comma seperated list and seperating by comma.
@@ -50,15 +52,17 @@ for bucket in s3_bucket.objects.all():
 
 cur = conn.cursor()
 
-file_path_collection = glob.glob('/home/ec2-user/sandwich_tweet%*')
+file_path_collection = glob.glob('/home/ec2-user/sandwich_tweet/sandwich_tweet%*')
 for file_path in file_path_collection:
 	with open(file_path) as file:
-		try:
+			print("about to read file")
 			tweet_info = file.read()
+			print("About to split file")
 			tweet_list = tweet_info.split('^&*>*&^')
-			tweet_list[3] = int(tweet_list[3])
+			print("about to convert id to int")
+			tweet_list[0] = int(tweet_list[0])
+			print("About to execute SQL")
 			cur.execute(sql_statement, tweet_list)
+			print("about to write a record")
 			conn.commit()
-		except:
-			print("this file is an issue: " + tweet_list[1])
 cur.close()
